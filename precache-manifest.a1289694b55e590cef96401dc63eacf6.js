be21e044c1a8e5bd7075e2847ee3d1e3a24822fc
Amit Singh <amsing52@in.ibm.com>
2020-06-14 21:50:49 +0530

    Updates

---
+++ precache-manifest.a1289694b55e590cef96401dc63eacf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d00c0c5c9a6ed1121da69e1f9841d7",
+    "url": "/github.io/ReactMonsters/index.html"
+  },
+  {
+    "revision": "da5876b7d7f0cea7d775",
+    "url": "/github.io/ReactMonsters/static/css/main.34091aea.chunk.css"
+  },
+  {
+    "revision": "2c643acc1b1026722189",
+    "url": "/github.io/ReactMonsters/static/js/2.f9462cd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/ReactMonsters/static/js/2.f9462cd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5876b7d7f0cea7d775",
+    "url": "/github.io/ReactMonsters/static/js/main.7194275f.chunk.js"
+  },
+  {
+    "revision": "c5b2f9a6cfd657d01dbf",
+    "url": "/github.io/ReactMonsters/static/js/runtime-main.c82216c9.js"
+  }
+]);
